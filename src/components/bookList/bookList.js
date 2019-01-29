86872a55a2a0ca19b1ba227de885c0df6b368b5e
Eugene <Jeka1.1@mail.ru>
2019-01-29 09:48:28 +0300

    redux store

--- src/components/bookList/bookList.js
+++ src/components/bookList/bookList.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { withBookstoreService } from '../hoc'
+import { booksLoaded } from '../../actions'
+
 import BookListItem from '../bookListItem/bookListItem'
 import './bookList.css'
 
 class BookList extends Component{
+  componentDidMount () {
+    const { bookstoreService } = this.props
+    const data = bookstoreService.getBooks()
+
+    this.props.booksLoaded(data)
+  }
+
   render () {
     const { books } = this.props
     return(
@@ -21,4 +33,32 @@ class BookList extends Component{
   }
 }
 
-export default BookList
+const mapStateToProps = ({ books }) => {
+  return { books }
+}
+                    // (dispatch) =>
+const mapDispatchToProps = {
+
+  // return{
+  //   booksLoaded: (newBooks) => {
+  //     dispatch({
+  //       type: 'BOOKS_LOADED',
+  //       payload: newBooks
+  //     })
+  //   }
+  // }
+
+  // return {
+  //   booksLoaded: (newBooks) => {
+  //     dispatch(booksLoaded(newBooks))
+  //   }
+  // }
+
+  // return bindActionCreators({
+  //   booksLoaded
+  // }, dispatch)
+
+  booksLoaded
+}
+
+export default withBookstoreService()(connect(mapStateToProps, mapDispatchToProps)(BookList))
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,12 +2,8 @@ import React from 'react'
 import BookList from '../bookList/bookList'
 
 const HomePage = () => {
-  const books = [
-    { id: 1, title: 'Production-ready Microservices', author: 'Susan J. Flower'},
-    { id: 2, title: 'Release It!', author: 'Michael T. Nygard!'},
-  ]
   return (
-    <BookList books={books}/>
+    <BookList />
 
   )
 }
